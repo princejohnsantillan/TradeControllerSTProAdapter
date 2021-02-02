dfdb73f1bff85bff56357b1c5076866a829792c2
Prince <mail@princejohnsantillan.net>
2021-02-02 23:58:07 +0800

    v0.17 - Versioning fix

--- TradeControllerSTProAdapter/My Project/AssemblyInfo.vb
+++ TradeControllerSTProAdapter/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.15")>
-<Assembly: AssemblyFileVersion("0.15")>
+<Assembly: AssemblyVersion("0.17")>
+<Assembly: AssemblyFileVersion("0.17")>
